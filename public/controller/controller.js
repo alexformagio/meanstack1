1034989640e4da5de274a60f92a8e99f4f7a1b99
alexformagio <alexandreformagio14@gmail.com>
2015-02-11 16:39:49 -0200

    finish 11022015

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -4,11 +4,51 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('AppController',['$scope','$http',function($scope,$http){
  console.log('Hello world from controller');
+    $scope.insertStatus = true;
+
+    var updateInArray = function(contact){
+        var id = contact._id;
+        for(i=0;i<$scope.contactList.length;i++){
+            var reg = $scope.contactList[i]._id;
+            console.log('updateinArray -> '+ reg +' - '+id);
+            if(reg == id) {
+                $scope.contactList[i]=contact;
+                break;
+            }
+        }
+
+    }
+
+    var deleteInArray = function(id){
+        for(i=0;i<$scope.contactList.length;i++){
+            var reg = $scope.contactList[i]._id;
+            console.log('deleteinArray -> '+ reg +' - '+id);
+            if(reg == id) {
+                $scope.contactList.splice(i, 1);
+                break;
+            }
+        }
+
+    }
+
+    $scope.editContact = function(id){
+        $scope.insertStatus=false;
+        for(i=0;i<$scope.contactList.length;i++){
+            var reg = $scope.contactList[i]._id;
+
+            if(reg == id) {
+                $scope.contact = $scope.contactList[i];
+                break;
+            }
+        }
+    }
 
     $scope.addContact = function(){
         console.log($scope.contact);
         $http.post('/contactlist',$scope.contact).success(function(response){
             console.log(response);
+            $scope.contactList.push(response);
+            $scope.contact = null;
         });
     };
 
@@ -16,5 +56,26 @@ myApp.controller('AppController',['$scope','$http',function($scope,$http){
         console.log('I received de data I requested');
         $scope.contactList = response;
     });
+
+    $scope.removeContact = function(id){
+        console.log('step1 -> '+id);
+
+        $http.delete('/contactlist/'+id).success(function(response){
+            console.log('confirmed delete -> '+response);
+            deleteInArray(id);
+            $scope.contact = null;
+        });
+    }
+
+    $scope.updateContact = function(){
+        console.log($scope.contact);
+        $http.put('/contactlist',$scope.contact).success(function(response){
+            console.log('confirmed update -> '+response);
+            updateInArray(response);
+            $scope.contact = null;
+            $scope.insertStatus = true;
+        });
+    }
+
 
 }]);
--- server.js
+++ server.js
@@ -25,39 +25,33 @@ app.get('/contactlist',function(req, res){
         res.json(docs);
     });
 
-    /*var person1 = {
-        name:'Alexandre',
-        email: 'alex@email.com',
-        number:'(11)1111-1111'
-    };
-
-    var person2 = {
-        name:'Thais',
-        email:'thais@email.com',
-        number:'(22)2222-2222'
-    };
-
-    var person3 = {
-        name:'Mariana',
-        email:'mariana@email.com',
-        number:'(33)3333-3333'
-    };
-
-    var person4 ={
-        name:'Evelyn',
-        email:'evelyn@email.com',
-        number:'(44)4444-4444'
-    };
-
-    var contactlist = [person1,person2,person3,person4];
-    res.json(contactlist);*/
 });
 
 app.post('/contactlist',function(req,res){
     console.log(req.body);
+    db.contactlist.insert(req.body,function(err,doc){
+        res.json(doc);
+    });
 });
 
+app.delete('/contactlist/:id',function(req,res){
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    });
+});
 
+app.put('/contactlist',function(req, res){
+    var contact = req.body;
+    console.log('update received -> ' + contact._id);
+    db.contactlist.findAndModify({query: {_id : mongojs.ObjectId(contact._id)},
+                                  update: {$set: {name: contact.name, email: contact.email, number: contact.number}},
+                                  new: true}, function(err, doc){
+        console.log('update responded -> ' + doc);
+        res.json(doc);
+    });
+});
 
 app.listen(3000);
 console.log('Server running on port 3000');
